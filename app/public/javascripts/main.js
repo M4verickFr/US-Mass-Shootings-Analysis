3c8b34fd0a778ebc17ea56ed29021f7b36890307
Maverick Perrollaz <maverick.perrollaz@pm.me>
2022-01-05 09:46:36 +0100

    add leaflet map

--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -1,30 +1,33 @@
-document.onload = function(){
-    var width = 400, height = 400;
-    var data = [100, 120, 140, 160, 180, 200];
-    var svg = d3.select("body")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
+var map = new L.Map("map", {center: [37.8, -96.9], zoom: 4})
+    .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
+var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-    var xscale = d3.scaleLinear()
-        .domain([0, d3.max(data)])
-        .range([0, width - 100]);
 
-    var yscale = d3.scaleLinear()
-        .domain([0, d3.max(data)])
-        .range([height/2, 0]);
+function projectPoint(x, y) {
+    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+    this.stream.point(point.x, point.y);
+}
 
-    var x_axis = d3.axisBottom().scale(xscale);
+d3.csv("data/Mass Shootings Dataset.csv", function(data){
+    console.log(data);
+    var transform = d3.geo.transform({point: projectPoint}),
+    path = d3.geo.path().projection(transform);
+});
 
-    var y_axis = d3.axisLeft().scale(yscale);
 
-    svg.append("g")
-        .attr("transform", "translate(50, 10)")
-        .call(y_axis);
 
-    var xAxisTranslate = height/2 + 10;
 
-    svg.append("g")
-        .attr("transform", "translate(50, " + xAxisTranslate  +")")
-        .call(x_axis)
-}();
+
+
+// Resize SVG
+var bounds = path.bounds(collection),
+    topLeft = bounds[0],
+    bottomRight = bounds[1];
+
+svg.attr("width", bottomRight[0] - topLeft[0])
+    .attr("height", bottomRight[1] - topLeft[1])
+    .style("left", topLeft[0] + "px")
+    .style("top", topLeft[1] + "px");
+
+g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
